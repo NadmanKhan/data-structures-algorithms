384f0a927806f5db4b98065175fa651fd22214f9
Nadman A. Khan <nadman.ash.khan@gmail.com>
2021-06-10 11:29:51 +0600

    Changed template identifiers.

--- data-structures/hash-map/hashmap.h
+++ data-structures/hash-map/hashmap.h
@@ -3,7 +3,7 @@
 
 namespace dsa {
 
-template <typename Key, typename Value>
+template <typename K, typename V>
 class Hash_map {
 public:
   explicit Hash_map(size_t size = 4, double max_load_factor = 0.75);
@@ -11,11 +11,11 @@ public:
   void clear();
   virtual ~Hash_map();
   Hash_map& operator=(const Hash_map& rhs);
-  Value& operator[](const Key& k);
-  void insert(const Key& k, const Value& v);
-  void set(const Key& k, const Value& v);
-  void remove(const Key& k);
-  bool contains(const Key& k) const;
+  V& operator[](const K& key);
+  void insert(const K& key, const V& value);
+  void set(const K& key, const V& value);
+  void remove(const K& key);
+  bool contains(const K& key) const;
   bool operator==(const Hash_map& rhs) const;
   size_t size() const;
   size_t capacity() const;
@@ -27,19 +27,21 @@ private:
   static constexpr double maxlf_max = 0.75;
   static constexpr double maxlf_min = 0.20;
 
-  enum Mark { null, live, dead };
+  enum Mark { null,
+    live,
+    dead };
   size_t size_;
   size_t capacity_;
-  Key* keys_;
-  Value* values_;
+  K* keys_;
+  V* values_;
   Mark* marks_;
   double max_load_factor_;
   size_t threshold_size_;
-  std::hash<Key> hasher_;
+  std::hash<K> hasher_;
 
   size_t& normalize_capacity(size_t& capacity) const;
   double& normalize_maxlf(double& maxlf) const;
-  size_t seek(const Key& k) const;
+  size_t seek(const K& key) const;
   void increase_capacity();
 };
 
@@ -49,25 +51,25 @@ constexpr double Hash_map<KeyType, ValueType>::maxlf_max;
 template <typename KeyType, typename ValueType>
 constexpr double Hash_map<KeyType, ValueType>::maxlf_min;
 
-template <typename Key, typename Value>
-Hash_map<Key, Value>::Hash_map(size_t size, double max_load_factor) :
+template <typename K, typename V>
+Hash_map<K, V>::Hash_map(size_t size, double max_load_factor) :
     size_(0),
     capacity_(normalize_capacity(size)),
     max_load_factor_(normalize_maxlf(max_load_factor)),
     threshold_size_(max_load_factor_ * capacity_),
-    keys_(new Key[capacity_]()),
-    values_(new Value[capacity_]()),
+    keys_(new K[capacity_]()),
+    values_(new V[capacity_]()),
     marks_(new Mark[capacity_]()),
-    hasher_(std::hash<Key>()) {}
+    hasher_(std::hash<K>()) {}
 
-template <typename Key, typename Value>
-Hash_map<Key, Value>::Hash_map(const Hash_map& other) :
+template <typename K, typename V>
+Hash_map<K, V>::Hash_map(const Hash_map& other) :
     size_(other.size_),
     capacity_(other.capacity_),
     max_load_factor_(other.max_load_factor_),
     threshold_size_(max_load_factor_ * capacity_),
-    keys_(new Key[capacity_]()),
-    values_(new Value[capacity_]()),
+    keys_(new K[capacity_]()),
+    values_(new V[capacity_]()),
     marks_(new Mark[capacity_]()),
     hasher_(other.hasher_) {
 
@@ -78,8 +80,8 @@ Hash_map<Key, Value>::Hash_map(const Hash_map& other) :
   }
 }
 
-template <typename Key, typename Value>
-inline size_t& Hash_map<Key, Value>::normalize_capacity(size_t& cap) const {
+template <typename K, typename V>
+inline size_t& Hash_map<K, V>::normalize_capacity(size_t& cap) const {
   if (cap < 4) {
     cap = 4;
   } else if (((cap - 1) & cap) != 0) {
@@ -88,9 +90,8 @@ inline size_t& Hash_map<Key, Value>::normalize_capacity(size_t& cap) const {
   return cap;
 }
 
-template <typename Key, typename Value>
-inline double&
-Hash_map<Key, Value>::normalize_maxlf(double& maxlf) const {
+template <typename K, typename V>
+inline double& Hash_map<K, V>::normalize_maxlf(double& maxlf) const {
   if (maxlf < maxlf_min) {
     maxlf = maxlf_min;
   } else if (maxlf > maxlf_max) {
@@ -99,21 +100,21 @@ Hash_map<Key, Value>::normalize_maxlf(double& maxlf) const {
   return maxlf;
 }
 
-template <typename Key, typename Value>
-inline size_t Hash_map<Key, Value>::seek(const Key& k) const {
+template <typename K, typename V>
+inline size_t Hash_map<K, V>::seek(const K& key) const {
   auto keep_bits = capacity_ - 1;
-  auto index = hasher_(k) & keep_bits;
-  while (keys_[index] != k && marks_[index] != null) {
+  auto index = hasher_(key) & keep_bits;
+  while (keys_[index] != key && marks_[index] != null) {
     index = (index + 1) & keep_bits;
   }
   return index;
 }
 
-template <typename Key, typename Value>
-void Hash_map<Key, Value>::increase_capacity() {
+template <typename K, typename V>
+void Hash_map<K, V>::increase_capacity() {
   auto tmp_cap = (capacity_ == 0) ? 4 : (capacity_ << 1);
-  auto tmp_keys = new Key[tmp_cap]();
-  auto tmp_values = new Value[tmp_cap]();
+  auto tmp_keys = new K[tmp_cap]();
+  auto tmp_values = new V[tmp_cap]();
   auto tmp_marks = new Mark[tmp_cap]();
   auto keep_bits = tmp_cap - 1;
   for (int i = 0; i < capacity_; i++) {
@@ -132,26 +133,27 @@ void Hash_map<Key, Value>::increase_capacity() {
                       (max_load_factor_ * capacity_));
 }
 
-template <typename Key, typename Value>
-inline void Hash_map<Key, Value>::clear() {
+template <typename K, typename V>
+inline void Hash_map<K, V>::clear() {
   delete[] keys_, delete[] values_, delete[] marks_;
-  std::tie(keys_, values_, marks_) = std::make_tuple(nullptr, nullptr, nullptr);
+  std::tie(keys_, values_, marks_) =
+      std::make_tuple(nullptr, nullptr, nullptr);
   std::tie(size_, capacity_, threshold_size_, max_load_factor_) =
       std::make_tuple(0, 0, 0, 0);
 }
 
-template <typename Key, typename Value>
-Hash_map<Key, Value>::~Hash_map() { clear(); }
+template <typename K, typename V>
+Hash_map<K, V>::~Hash_map() { clear(); }
 
-template <typename Key, typename Value>
-inline Hash_map<Key, Value>&
-Hash_map<Key, Value>::operator=(const Hash_map<Key, Value>& rhs) {
+template <typename K, typename V>
+inline Hash_map<K, V>& Hash_map<K, V>::operator=(const Hash_map<K, V>& rhs) {
   if (this != &rhs) {
     clear();
     std::tie(size_, capacity_, threshold_size_, max_load_factor_) =
-        std::make_tuple(rhs.size_, rhs.capacity_, rhs.threshold_size_, rhs.max_load_factor_);
-    keys_ = new Key[capacity_]();
-    values_ = new Value[capacity_]();
+        std::make_tuple(rhs.size_, rhs.capacity_,
+                        rhs.threshold_size_, rhs.max_load_factor_);
+    keys_ = new K[capacity_]();
+    values_ = new V[capacity_]();
     marks_ = new Mark[capacity_]();
     for (int i = 0; i < capacity_; i++) {
       std::tie(keys_[i], values_[i], marks_[i]) =
@@ -161,44 +163,44 @@ Hash_map<Key, Value>::operator=(const Hash_map<Key, Value>& rhs) {
   return *this;
 }
 
-template <typename Key, typename Value>
-inline Value& Hash_map<Key, Value>::operator[](const Key& k) {
-  auto index = seek(k);
+template <typename K, typename V>
+inline V& Hash_map<K, V>::operator[](const K& key) {
+  auto index = seek(key);
   if (marks_[index] == null) {
     while (size_ >= threshold_size_) {
       increase_capacity();
     }
-    keys_[index] = k;
+    keys_[index] = key;
     marks_[index] = live;
     size_++;
   }
   return values_[index];
 }
 
-template <typename Key, typename Value>
-inline void Hash_map<Key, Value>::insert(const Key& k, const Value& v) {
-  operator[](k) = v;
+template <typename K, typename V>
+inline void Hash_map<K, V>::insert(const K& key, const V& value) {
+  operator[](key) = value;
 }
 
-template <typename Key, typename Value>
-inline void Hash_map<Key, Value>::set(const Key& k, const Value& v) { insert(k, v); }
+template <typename K, typename V>
+inline void Hash_map<K, V>::set(const K& key, const V& value) { insert(key, value); }
 
-template <typename Key, typename Value>
-inline void Hash_map<Key, Value>::remove(const Key& k) {
-  auto index = seek(k);
+template <typename K, typename V>
+inline void Hash_map<K, V>::remove(const K& key) {
+  auto index = seek(key);
   if (marks_[index] == live) {
     marks_[index] = dead;
     size_--;
   }
 }
 
-template <typename Key, typename Value>
-inline bool Hash_map<Key, Value>::contains(const Key& k) const {
-  return marks_[seek(k)] == live;
+template <typename K, typename V>
+inline bool Hash_map<K, V>::contains(const K& key) const {
+  return marks_[seek(key)] == live;
 }
 
-template <typename Key, typename Value>
-inline bool Hash_map<Key, Value>::operator==(const Hash_map<Key, Value>& rhs) const {
+template <typename K, typename V>
+inline bool Hash_map<K, V>::operator==(const Hash_map<K, V>& rhs) const {
   if (this == &rhs) {
     return true;
   }
@@ -216,22 +218,22 @@ inline bool Hash_map<Key, Value>::operator==(const Hash_map<Key, Value>& rhs) co
   return true;
 }
 
-template <typename Key, typename Value>
-inline size_t Hash_map<Key, Value>::size() const { return size_; }
+template <typename K, typename V>
+inline size_t Hash_map<K, V>::size() const { return size_; }
 
-template <typename Key, typename Value>
-inline size_t Hash_map<Key, Value>::capacity() const { return capacity_; }
+template <typename K, typename V>
+inline size_t Hash_map<K, V>::capacity() const { return capacity_; }
 
-template <typename Key, typename Value>
-inline double Hash_map<Key, Value>::current_load_factor() const {
+template <typename K, typename V>
+inline double Hash_map<K, V>::current_load_factor() const {
   return (capacity_ == 0) ? 0 : (static_cast<double>(size_) / capacity_);
 }
 
-template <typename Key, typename Value>
-inline double Hash_map<Key, Value>::max_load_factor() const { return max_load_factor_; }
+template <typename K, typename V>
+inline double Hash_map<K, V>::max_load_factor() const { return max_load_factor_; }
 
-template <typename Key, typename Value>
-inline void Hash_map<Key, Value>::set_max_load_factor(double x) {
+template <typename K, typename V>
+inline void Hash_map<K, V>::set_max_load_factor(double x) {
   max_load_factor_ = normalize_maxlf(x);
   threshold_size_ = max_load_factor_ * capacity_;
   if (size_ >= threshold_size_) {
